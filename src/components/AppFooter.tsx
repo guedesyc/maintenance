f94fb9c21311a76999c16302d20e42e9a41d04e4
Bowie <guedesymc@gmail.com>
2026-07-29 15:11:38 -0300

    fix: link YG Systems footer to Instagram

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -3,7 +3,7 @@ export default function AppFooter() {
     <footer className="py-6 text-center text-sm text-stone-500">
       <a
         className="font-medium text-stone-600 transition hover:text-brand-700"
-        href="https://instagram.com/guedesyc"
+        href="https://instagram.com/yg.systems"
         target="_blank"
         rel="noreferrer"
       >
